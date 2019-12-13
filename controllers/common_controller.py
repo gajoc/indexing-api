729a75cdbf7477dddaff1ecc25ce7fdf9923bda2
pawel <pawel.maciejski@gmail.com>
2019-12-13 23:05:00 +0100

    #16 extract common interface in controllers

---
+++ controllers/common_controller.py
@@ -0,0 +1,35 @@
+from abc import abstractmethod
+from typing import Dict
+
+from controllers.icontroller import IController
+from utils.autocomplete import AutocompleteFields
+from utils.browser import Browser
+from utils.constants import INPUT_FIELDS, STORAGE_DIR, SELENIUM_CONFIG, NEXT_BUTTON_FAMILY_SEARCH, USER_BROWSER, \
+    AUTOCOMPLETE_FIELDS, UserAction
+from utils.storage import Storage
+
+
+class CommonController(IController):
+
+    def __init__(self):
+        self._autocomplete = AutocompleteFields(fields=AUTOCOMPLETE_FIELDS)
+        self._browser = Browser(user_browser=USER_BROWSER,
+                                next_button=NEXT_BUTTON_FAMILY_SEARCH,
+                                previous_button=None,
+                                config=SELENIUM_CONFIG)
+        self._input_fields = INPUT_FIELDS
+        self._storage = Storage(STORAGE_DIR)
+
+    @abstractmethod
+    def wait_for_user_action(self) -> UserAction:
+        pass
+
+    @abstractmethod
+    def execute(self, action: UserAction):
+        pass
+
+    def before_exit(self) -> None:
+        self._storage.dump()
+
+    def _add_browser_link(self, entity: Dict) -> None:
+        entity['scan_link'] = self._browser.current_url()
--- controllers/voice_controller.py
+++ controllers/voice_controller.py
@@ -1,33 +1,20 @@
-from typing import Union, Dict
+from typing import Union
 
-from controllers.icontroller import IController
-from utils.autocomplete import AutocompleteFields
-from utils.browser import Browser
-from utils.constants import UserAction, COMMAND_2_ACTION, SPEECH_2_COMMAND, AUTOCOMPLETE_FIELDS, INPUT_FIELDS, \
-    USER_BROWSER, NEXT_BUTTON_FAMILY_SEARCH, SELENIUM_CONFIG, STORAGE_DIR
+from controllers.common_controller import CommonController
+from utils.constants import UserAction, COMMAND_2_ACTION, SPEECH_2_COMMAND
 from utils.mic import get_voice_command, collect_user_inputs
-from utils.storage import Storage
 
 
-class VoiceController(IController):
+class VoiceController(CommonController):
 
     def __init__(self):
+        super().__init__()
         self._to_command = SPEECH_2_COMMAND
-        self._autocomplete = AutocompleteFields(fields=AUTOCOMPLETE_FIELDS)
-        self._browser = Browser(user_browser=USER_BROWSER,
-                                next_button=NEXT_BUTTON_FAMILY_SEARCH,
-                                previous_button=None,
-                                config=SELENIUM_CONFIG)
-        self._input_fields = INPUT_FIELDS
-        self._storage = Storage(STORAGE_DIR)
 
     def wait_for_user_action(self) -> UserAction:
         speech_recognized = get_voice_command()
         command = self._to_command.get(speech_recognized)
         return COMMAND_2_ACTION.get(command)
-
-    def before_exit(self) -> None:
-        self._storage.dump()
 
     def execute(self, action: UserAction) -> Union[UserAction, None]:
         if action == UserAction.DATA_INPUT:
@@ -40,6 +27,3 @@ class VoiceController(IController):
             self._browser.click_next()
         else:
             print(f'Nierozpoznano akcji {action}')
-
-    def _add_browser_link(self, entity: Dict) -> None:
-        entity['scan_link'] = self._browser.current_url()
