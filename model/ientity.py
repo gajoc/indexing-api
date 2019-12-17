94c9497a8e6bb88def336d7abca076bf1e09fc47
pawel <pawel.maciejski@gmail.com>
2019-12-17 23:40:37 +0100

    add schema for short entity (to implement later)

--- model/ientity.py
+++ model/ientity.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_dump
 from marshmallow_enum import EnumField
 
 from utils.constants import UserAction, DATETIME_FORMAT
@@ -16,3 +16,11 @@ class IEntitySchema(Schema):
 class MilitaryEntityFamilySearchSchema(IEntitySchema):
     surname = fields.Str(required=True, attribute='nazwisko', data_key='nazwisko')
     born_year = fields.Str(required=True, attribute='data_ur', data_key='data_ur')
+
+
+class MilitaryShortEntityFamilySearchSchema(MilitaryEntityFamilySearchSchema):
+
+    @post_dump
+    def short_entity(self, data, **kwargs):
+        raise NotImplementedError('Implementation from misc.short_print needed here. Use it in project.')
+        pass
