5087914e6740366b98d22f7ea90225da5876191c
pawel <pawel.maciejski@gmail.com>
2019-12-17 22:54:56 +0100

    add short print function of entity

--- controllers/fs/action.py
+++ controllers/fs/action.py
@@ -1,8 +1,8 @@
-from typing import Union
+from typing import Union, Dict
 
 from controllers.iaction import IAction
 from utils.constants import UserAction
-from utils.misc import collect_user_inputs, add_info
+from utils.misc import collect_user_inputs, add_info, short_print
 
 
 class FamilySearchOnePageOneManAction(IAction):
@@ -13,25 +13,24 @@ class FamilySearchOnePageOneManAction(IAction):
     def execute(self, action: UserAction) -> Union[UserAction, None]:
         if action == UserAction.DATA_INPUT:
             entity = collect_user_inputs(self._input_fields, autocomplete=self._autocomplete)
-            self._add_browser_link(entity)
-            add_info(entity, value=UserAction.DATA_INPUT.name)
-            self._storage.add(entity)
-            print(entity)
+            self._prepare_and_add_to_storage(entity, UserAction.DATA_INPUT.name)
+            short_print(entity)
             return UserAction.NEXT_SCAN
         elif action == UserAction.NEXT_SCAN:
             self._browser.click_next()
         elif action == UserAction.COPY:
             entity = self._storage.get_previous_copied()
-            self._add_browser_link(entity)
-            add_info(entity, value=UserAction.COPY.name)
-            self._storage.add(entity)
+            self._prepare_and_add_to_storage(entity, UserAction.COPY.name)
+            short_print(entity)
             return UserAction.NEXT_SCAN
         elif action == UserAction.UNREADABLE:
             entity = {}
-            self._add_browser_link(entity)
-            add_info(entity, value=UserAction.UNREADABLE.name)
-            self._storage.add(entity)
-            print(entity)
+            self._prepare_and_add_to_storage(entity, UserAction.UNREADABLE.name)
             return UserAction.NEXT_SCAN
         else:
             print(f'Nieznana akcja, proszę wybrać jedną z dostępnych akcji.')
+
+    def _prepare_and_add_to_storage(self, entity: Dict, action_name: str) -> None:
+        self._add_browser_link(entity)
+        add_info(entity, value=action_name)
+        self._storage.add(entity)
--- utils/misc.py
+++ utils/misc.py
@@ -1,3 +1,4 @@
+import math
 from typing import Sequence, Type, Dict
 
 from utils.autocomplete import AutocompleteFields
@@ -15,3 +16,15 @@ def collect_user_inputs(fields: Sequence, autocomplete: Type[AutocompleteFields]
 
 def add_info(entity: Dict, value) -> None:
     entity['info'] = value
+
+
+def asterisk_string(value: str, parts: int) -> str:
+    x = math.ceil(len(value)/parts)
+    return f'{value[:x]}***{value[-x:]}'
+
+
+def short_print(entity: Dict, values_only: bool = True, link_parts: int = 5) -> None:
+    short_entity = entity.copy()
+    short_entity.pop('created_utc')
+    short_entity['scan_link'] = asterisk_string(short_entity['scan_link'], parts=link_parts)
+    print(tuple(short_entity.values()) if values_only else short_entity)
