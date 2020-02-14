c0bc13d5a036071ddf8c15b3d479ef47ef3d5814
pawel <pawel.maciejski@gmail.com>
2020-02-14 19:31:56 +0100

    refactor: next action configurable in action class

--- controllers/fs/action.py
+++ controllers/fs/action.py
@@ -19,7 +19,6 @@ class FamilySearchOnePageOneManAction(IAction):
             self._prepare_and_add_to_storage(entity, UserAction.DATA_INPUT)
             human_view = self.human_readable_schema.dump(entity)
             print(human_view)
-            return UserAction.NEXT_SCAN
         elif action == UserAction.NEXT_SCAN:
             self._browser.click_next()
         elif action == UserAction.COPY:
@@ -27,11 +26,9 @@ class FamilySearchOnePageOneManAction(IAction):
             self._prepare_and_add_to_storage(entity, UserAction.COPY)
             human_view = self.human_readable_schema.dump(entity)
             print(human_view)
-            return UserAction.NEXT_SCAN
         elif action == UserAction.UNREADABLE:
             entity = {}
             self._prepare_and_add_to_storage(entity, UserAction.UNREADABLE)
-            return UserAction.NEXT_SCAN
         elif action == UserAction.PREV_SCAN:
             entity = self._storage.pop_previous()
             human_view = self.human_readable_schema.dump(entity) if entity else {}
@@ -39,6 +36,17 @@ class FamilySearchOnePageOneManAction(IAction):
             self._browser.click_previous()
         else:
             print(f'Nieznana akcja, proszę wybrać jedną z dostępnych akcji.')
+        return self.next_action(action)
+
+    def next_action(self, action: UserAction) -> Union[UserAction, None]:
+        mapper = {
+            UserAction.DATA_INPUT: UserAction.NEXT_SCAN,
+            UserAction.NEXT_SCAN: None,
+            UserAction.COPY: UserAction.NEXT_SCAN,
+            UserAction.UNREADABLE: UserAction.NEXT_SCAN,
+            UserAction.PREV_SCAN: None
+        }
+        return mapper.get(action)
 
     def _prepare_and_add_to_storage(self, entity: Dict, action: UserAction) -> None:
         self._add_browser_link(entity, key='scan_link')
--- controllers/iaction.py
+++ controllers/iaction.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Union
 
 from utils.constants import UserAction
 
@@ -7,4 +8,8 @@ class IAction(ABC):
 
     @abstractmethod
     def execute(self, action: UserAction):
+        pass
+
+    @abstractmethod
+    def next_action(self, action: UserAction) -> Union[UserAction, None]:
         pass
