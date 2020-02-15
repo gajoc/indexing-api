1f300cac844e70b33003c253ffcd8147a22dd873
pawel <pawel.maciejski@gmail.com>
2020-02-15 23:18:26 +0100

    #21 move config to json

--- run.py
+++ run.py
@@ -1,17 +1,9 @@
+import json
+
 from create import create_app
 from utils.constants import GenealogyService
 
-config = {
-    "storage_dir": "data",
-    "storage_entities_limit": 100,
-    "browser": {
-        "name": "chrome",
-        "driverPath": "bin/chromedriver",
-        "experimentalOptions": {
-            "debuggerAddress": "127.0.0.1:9222",
-        }
-    }
-}
 
+config = json.load(open("config.json", encoding="utf-8"))
 app = create_app(service=GenealogyService.FAMILY_SEARCH, config=config)
 app.run()
