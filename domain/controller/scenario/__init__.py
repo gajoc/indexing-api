d3a1779b7984963164a607f1a61ef4141f071b0e
pawel <pawel.maciejski@gmail.com>
2020-02-22 21:02:27 +0100

    refactor: ScenarioControllers as registry

--- domain/controller/scenario/__init__.py
+++ domain/controller/scenario/__init__.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from typing import DefaultDict, Dict
+
 from domain.controller.icontroller import IController
 from domain.controller.scenario.fs_military_records import FamilySearchMilitaryRecordsKeyboardCommand, \
     FamilySearchMilitaryRecordsVoiceCommand
@@ -5,14 +8,19 @@ from utils.constants import InteractionMethod, GenealogyDocumentType
 
 
 class ScenarioControllers:
-    @staticmethod
-    def get(method: InteractionMethod, document_type: GenealogyDocumentType) -> IController:
-        mapper = {
-            InteractionMethod.KEYBOARD: {
-                GenealogyDocumentType.MILITARY_RECORD: FamilySearchMilitaryRecordsKeyboardCommand
-            },
-            InteractionMethod.VOICE: {
-                GenealogyDocumentType.MILITARY_RECORD: FamilySearchMilitaryRecordsVoiceCommand
-            },
-        }
-        return mapper[method][document_type]
+
+    mapper: DefaultDict[InteractionMethod, Dict[GenealogyDocumentType, IController]] = defaultdict(dict)
+
+    @classmethod
+    def register(cls, method: InteractionMethod, document_type: GenealogyDocumentType, scenario: IController) -> None:
+        cls.mapper[method][document_type] = scenario
+
+    @classmethod
+    def get(cls, method: InteractionMethod, document_type: GenealogyDocumentType) -> IController:
+        return cls.mapper[method][document_type]
+
+
+ScenarioControllers.register(InteractionMethod.KEYBOARD, GenealogyDocumentType.MILITARY_RECORD,
+                             FamilySearchMilitaryRecordsKeyboardCommand)
+ScenarioControllers.register(InteractionMethod.VOICE, GenealogyDocumentType.MILITARY_RECORD,
+                             FamilySearchMilitaryRecordsVoiceCommand)
